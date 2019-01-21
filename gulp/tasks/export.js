c07e3741981b6c0668efcaf82715ad4455c8cf2f
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-01-20 22:03:58 -0500

    change zip path to root

--- gulp/tasks/export.js
+++ gulp/tasks/export.js
@@ -2,12 +2,12 @@ const gulp = require('gulp'),
       zip = require('gulp-zip'),
       del = require('del');
 
-gulp.task('deleteZIP', () => del('./zip'));
+gulp.task('deleteZIP', () => del('./dist.zip'));
 
 gulp.task('copy', function(done) {
   gulp.src('dist/**')
     .pipe(zip('dist.zip'))
-    .pipe(gulp.dest('zip'));
+    .pipe(gulp.dest('./'));
     done();
   }
 );
