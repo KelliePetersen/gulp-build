0dac83c5a5e873fc23aaa38fb2bd198d831422a1
KelliePetersen <KellieAnnPetersen@gmail.com>
2019-08-20 20:03:39 -0400

    add rfs, global css, and menu item

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -3,11 +3,21 @@ const gulp = require('gulp'),
       autoprefixer = require('autoprefixer'),
       nested = require('postcss-nested'),
       cssImport = require('postcss-import'),
-      mixins = require('postcss-mixins');
+      mixins = require('postcss-mixins'),
+      rfs = require('rfs');
+
+const options = {
+  twoDimensional: true,
+  baseFontSize: 16,
+  fontSizeUnit: 'rem',
+  breakpoint: 768,
+  breakpointUnit: 'px',
+  factor: 40
+};
 
 gulp.task('buildCSS', () => 
   gulp.src('./app/src/css/styles.css')
-    .pipe(postcss([cssImport, mixins, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, nested, autoprefixer, rfs(options)]))
     .on('error', function(error) {
       console.log(error.toString());
       this.emit('end');
